9ef621434ec05df2e09587a95d159ada977c60cd
Bob den Otter <bob@twokings.nl>
2015-11-02 17:13:02 +0100

    Updating README

--- Extension.php
+++ Extension.php
@@ -47,16 +47,16 @@ class Extension extends BaseExtension
     {
 
         $msg = "Welcome to the <strong>Chmodinator</strong>! If you're having
-        issues with your Bolt files not being removable using your FTP client or
-        vice-versa, this extension will help you sort it out. Clicking the 'Fix'
-        button will try to make all files in the data folders writable to 'all',
-        and it will inform you of any files it couldn't modify, so you can
-        change them using the command line or your (S)FTP client.<br><br>
+        issues with your Bolt files not being removable using your FTP client or vice-
+        versa, this extension will help you sort it out by applying 'chmod' to it.
+        Clicking the 'Fix' button will try to make all files in the data folders
+        writable to 'all', and it will inform you of any files it couldn't modify, so
+        you can change them using the command line or your (S)FTP client.<br><br>
 
-        We are aware that using this tool is not considered 'good practice'. If
-        possible, you should work with your system administrator to get things
-        set up properly. If that's not an option, of if you're on shared
-        hosting, this extension will help you out!";
+        You should be aware that using this tool is not considered 'good practice'. If
+        possible, you should work with your system administrator to get things set up
+        properly. If that's not an option, or if you're on shared hosting, this
+        extension will help you out!";
 
         $data = array('msg' => $msg);
 
