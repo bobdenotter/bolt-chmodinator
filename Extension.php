ffa582abf9ddc6cb35cb8a28b0d71f6498332ca2
Bob den Otter <bob@twokings.nl>
2015-11-02 17:03:30 +0100

    Working

--- Extension.php
+++ Extension.php
@@ -19,12 +19,9 @@ class Extension extends BaseExtension
         // Admin menu
         $this->addMenuOption('Chmodinator', $root . 'chmodinator', 'fa:hand-stop-o');
 
-        $this->app->get($root . 'chmodinator', array($this, 'index'))
-            ->bind('chmodinator')
-        ;
-        $this->app->get($root . 'chmodinator/check', array($this, 'check'))
-            ->bind('chmodinator-check')
-        ;
+        $this->app->get($root . 'chmodinator', array($this, 'index'))->bind('chmodinator');
+        $this->app->get($root . 'chmodinator/check', array($this, 'check'))->bind('chmodinator-check');
+        $this->app->get($root . 'chmodinator/fix', array($this, 'fix'))->bind('chmodinator-fix');
 
         $this->basepath = $this->app['resources']->getPath('root');
 
@@ -49,19 +46,33 @@ class Extension extends BaseExtension
     public function index()
     {
 
-        return $this->render('index.twig');
+        $msg = "Welcome to the <strong>Chmodinator</strong>! If you're having
+        issues with your Bolt files not being removable using your FTP client or
+        vice-versa, this extension will help you sort it out. Clicking the 'Fix'
+        button will try to make all files in the data folders writable to 'all',
+        and it will inform you of any files it couldn't modify, so you can
+        change them using the command line or your (S)FTP client.<br><br>
+
+        We are aware that using this tool is not considered 'good practice'. If
+        possible, you should work with your system administrator to get things
+        set up properly. If that's not an option, of if you're on shared
+        hosting, this extension will help you out!";
+
+        $data = array('msg' => $msg);
+
+        return $this->render('index.twig', $data);
 
     }
 
 
     public function check()
     {
-
+        // Clear the cache beforehand, ar this will make sure we have a lot less files to deal with.
         $result = $this->app['cache']->clearCache();
 
         $files = '';
         $finder = new Finder();
-        $finder->depth('<1');
+        $finder->depth('<4');
 
         foreach($this->locations as $loc) {
             $finder->in($loc);
@@ -72,7 +83,12 @@ class Extension extends BaseExtension
             $files .= $res;
         }
 
-        $data = array('files' => $files);
+        $msg = "Below you'll see the output of the checks. Lines marked with a
+        red '<i class='fa fa-close red'></i>' are not writable, and should be
+        fixed. For files owned by you, you should use the command-line or your
+        (S)FTP client to make sure these files are set correctly.";
+
+        $data = array('files' => $files, 'msg' => $msg);
 
         return $this->render('index.twig', $data);
 
@@ -80,23 +96,44 @@ class Extension extends BaseExtension
 
     public function fix()
     {
-
+        // Clear the cache beforehand, ar this will make sure we have a lot less files to deal with.
         $result = $this->app['cache']->clearCache();
 
         $files = '';
         $finder = new Finder();
-        $finder->depth('<1');
+        $finder->depth('<4');
 
         foreach($this->locations as $loc) {
             $finder->in($loc);
         }
 
         foreach ($finder as $file) {
-            $res = $this->getPrintInfoFile($file);
-            $files .= $res;
+
+            $perms = substr(sprintf('%o', $file->getPerms()), -3);
+
+            if ($file->isDir() && $perms != '777') {
+                @chmod($file, 0777);
+                if (!$file->isWritable()) {
+                    $res = $this->getPrintInfoFile($file);
+                    $files .= $res;
+                }
+            } else if (!$file->isDir() && !$this->checkFilePerms($perms)) {
+                // echo "joe! " . $this->checkFilePerms($perms);
+                @chmod($file, 0666);
+                $res = $this->getPrintInfoFile($file);
+                $files .= $res;
+            }
+
+
         }
 
-        $data = array('files' => $files);
+        $msg = "Below you'll see the output of the changes. If there are lines
+        left with a red '<i class='fa fa-close red'></i>', then these files /
+        folders could now be modified by Bolt or the Chmodinator. You should use
+        the command-line or your (S)FTP client to make sure these files are set
+        correctly.";
+
+        $data = array('files' => $files, 'msg' => $msg);
 
         return $this->render('index.twig', $data);
 
@@ -118,32 +155,46 @@ class Extension extends BaseExtension
     public function getPrintInfoFile($file)
     {
 
-        $path = str_replace($this->basepath, '…/', $file->getRealPath());
+        $path = str_replace($this->basepath, '…/', $file->getPathname());
         $dirname = dirname($path);
         $basename = basename($path);
 
-        $owner = $file->getOwner();
         try {
-            $owner_info = posix_getpwuid($owner);
-            $owner = $owner_info['name'];
+            $owner = @$file->getOwner();
+            try {
+                $owner_info = posix_getpwuid($owner);
+                $owner = $owner_info['name'];
+            } catch (\Exception $e) {
+                // nothing
+            }
         } catch (\Exception $e) {
-            // nothing
+            $owner = '-';
         }
 
-        $group = $file->getGroup();
+
         try {
-            $group_info = posix_getgrgid($group);
-            $group = $group_info['name'];
+            $group = @$file->getGroup();
+            try {
+                $group_info = posix_getgrgid($group);
+                $group = $group_info['name'];
+            } catch (\Exception $e) {
+                // nothing
+            }
         } catch (\Exception $e) {
-            // nothing
+            $group = '-';
         }
 
+        try {
+            $perms = substr(sprintf('%o', @$file->getPerms()), -3);
+        } catch (\Exception $e) {
+            $perms = "000";
+        }
 
         $res = sprintf("<i class='fa fa-fw fa-%s %s'></i> %-21s %-7s <i class='fa fa-fw fa-%s'></i> %s/<b>%s</b>\n",
             $file->isWritable() ? 'check' : 'close',
             $file->isWritable() ? 'green' : 'red',
             $owner . ':' . $group,
-            substr(sprintf('%o', $file->getPerms()), -4),
+            $perms,
             $file->isDir() ? 'folder-open-o' : 'file-o',
             $dirname,
             $basename
@@ -151,6 +202,14 @@ class Extension extends BaseExtension
 
 
         return $res;
+
+    }
+
+    private function checkFilePerms($perms)
+    {
+        $ok = array('666', '667', '676', '677', '766', '767', '776', '777');
+
+        return in_array($perms, $ok);
 
     }
 
